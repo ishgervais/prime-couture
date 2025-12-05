2e57c0b5873c8746c418b4f0710a5e1129b0e8c1
Gervais I <ishimwegervais11@gmail.com>
2025-12-05 03:13:19 -0500

    rm: removed git for clients

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,34 +1,34 @@
-import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
-import { APP_GUARD } from '@nestjs/core';
-import { AuthModule } from './auth/auth.module';
-import { CollectionsModule } from './collections/collections.module';
-import { CategoriesModule } from './categories/categories.module';
-import { ProductsModule } from './products/products.module';
-import { OrdersModule } from './orders/orders.module';
-import { AnalyticsModule } from './analytics/analytics.module';
-import { FilesModule } from './files/files.module';
-import { AppController } from './app.controller';
+import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler'
+import { APP_GUARD } from '@nestjs/core'
+import { AuthModule } from './auth/auth.module'
+import { CollectionsModule } from './collections/collections.module'
+import { CategoriesModule } from './categories/categories.module'
+import { ProductsModule } from './products/products.module'
+import { OrdersModule } from './orders/orders.module'
+import { AnalyticsModule } from './analytics/analytics.module'
+import { FilesModule } from './files/files.module'
+import { AppController } from './app.controller'
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-    ThrottlerModule.forRoot([{ ttl: 60, limit: 120 }]),
-    AuthModule,
-    CollectionsModule,
-    CategoriesModule,
-    ProductsModule,
-    OrdersModule,
-    AnalyticsModule,
-    FilesModule,
-  ],
-  controllers: [AppController],
-  providers: [
-    {
-      provide: APP_GUARD,
-      useClass: ThrottlerGuard,
-    },
-  ],
+    imports: [
+        ConfigModule.forRoot({ isGlobal: true }),
+        ThrottlerModule.forRoot([{ ttl: 60, limit: 120 }]),
+        AuthModule,
+        CollectionsModule,
+        CategoriesModule,
+        ProductsModule,
+        OrdersModule,
+        AnalyticsModule,
+        FilesModule,
+    ],
+    controllers: [AppController],
+    providers: [
+        {
+            provide: APP_GUARD,
+            useClass: ThrottlerGuard,
+        },
+    ],
 })
 export class AppModule {}
